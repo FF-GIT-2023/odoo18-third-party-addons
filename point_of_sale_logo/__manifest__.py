ce16bd55d6e6f5c5bdea79bf37acafe0f61c0c29
Vyshnav9062 <vyshnav9062@gmail.com>
2025-09-29 09:46:25 +0530

    [IMP]:point_of_sale_logo:pos return barcode

--- point_of_sale_logo/__manifest__.py
+++ point_of_sale_logo/__manifest__.py
@@ -37,6 +37,7 @@
         'point_of_sale._assets_pos': [
             'point_of_sale_logo/static/src/xml/navbar_logo.xml',
             'point_of_sale_logo/static/src/xml/receipt_header_logo.xml',
+            'point_of_sale_logo/static/src/js/pos_receipt.js',
             'point_of_sale_logo/static/src/xml/receipt_screen_logo.xml',
         ],
     },
