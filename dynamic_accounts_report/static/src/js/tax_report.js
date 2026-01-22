13f1f85ea06f6af3421070a5f7be76872ceef714
Vyshnav9062 <vyshnav9062@gmail.com>
2026-01-22 14:24:32 +0530

    [FIX]:dynamic_accounts_report:fixed tax report date filter issue

--- dynamic_accounts_report/static/src/js/tax_report.js
+++ dynamic_accounts_report/static/src/js/tax_report.js
@@ -84,14 +84,14 @@ class TaxReport extends owl.Component {
         }
         if (val && val.target.name === 'start_date') {
             this.state.date_viewed = []
-            this.state.date_viewed.push('From' + ' ' + this.formatDate(this.start_date.el.value) + ' ' + 'To' + ' ' + this.formatDate(this.end_date.el.value))
+//            this.state.date_viewed.push('From' + ' ' + this.formatDate(this.start_date.el.value) + ' ' + 'To' + ' ' + this.formatDate(this.end_date.el.value))
             this.state.date_range = {
                 ...this.state.date_range,
                 start_date: val.target.value
             };
         } else if (val && val.target.name === 'end_date') {
             this.state.date_viewed = []
-            this.state.date_viewed.push('From' + ' ' + this.formatDate(this.start_date.el.value) + 'To' + ' ' + this.formatDate(this.end_date.el.value))
+//            this.state.date_viewed.push('From' + ' ' + this.formatDate(this.start_date.el.value) + 'To' + ' ' + this.formatDate(this.end_date.el.value))
             this.state.date_range = {
                 ...this.state.date_range,
                 end_date: val.target.value
