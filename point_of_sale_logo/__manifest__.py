ba3e5189f5b437222811b1f61a9aab0a067b8a82
Vyshnav9062 <vyshnav9062@gmail.com>
2026-04-02 12:03:27 +0530

    [IMP]:point_of_sale_logo:changed receipt format

--- point_of_sale_logo/__manifest__.py
+++ point_of_sale_logo/__manifest__.py
@@ -39,6 +39,7 @@
             'point_of_sale_logo/static/src/xml/receipt_header_logo.xml',
             'point_of_sale_logo/static/src/js/pos_receipt.js',
             'point_of_sale_logo/static/src/js/get_partner.js',
+            'point_of_sale_logo/static/src/xml/order_receipt.xml',
             'point_of_sale_logo/static/src/xml/receipt_screen_logo.xml',
         ],
     },
